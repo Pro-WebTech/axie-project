08501e2697553909923b6856e93b520db2370625
Supersmile <baydev1998@gmail.com>
2021-08-09 12:02:21 +0900

    @

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors())
 
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname+'/frontend/build/index.html'));
   });
 
 
